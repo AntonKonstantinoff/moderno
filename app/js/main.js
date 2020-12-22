c38c73256048d5b519545ce0b4cef63792c8846d
AntonKonstantinoff <konstantinoff555@gmail.com>
2020-12-22 11:32:47 +0300

    add slider-products

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,19 @@ $(function () {
 
 	var mixer = mixitup('.product__items');
 
+	$('.products-slider__items').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		dots: true,
+		arrows: false
+	});
+
+	$(".star").rateYo({
+		starWidth: "15px",
+		rating: 5,
+		spacing: '3px',
+		readOnly: true
+	});
+
 });
