4d43ea9e93d14bbecf7795a4cc27bcf0176cb9f8
AntonKonstantinoff <konstantinoff555@gmail.com>
2021-01-26 22:56:28 +0300

    add popap

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,8 @@ gulp.task('script', function () {
 		'node_modules/slick-carousel/slick/slick.js',
 		'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
 		'node_modules/rateyo/dist/jquery.rateyo.js',
-		'node_modules/mixitup/dist/mixitup.js'
+		'node_modules/mixitup/dist/mixitup.js',
+		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
 	])
 		.pipe(concat('libs.min.js'))
 		.pipe(uglify())
@@ -36,7 +37,8 @@ gulp.task('style', function () {
 	return gulp.src([
 		'node_modules/normalize.css/normalize.css',
 		'node_modules/magnific-popup/dist/magnific-popup.css',
-		'node_modules/rateyo/dist/jquery.rateyo.css'
+		'node_modules/rateyo/dist/jquery.rateyo.css',
+		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
 	])
 		.pipe(concat('libs.min.css'))
 		.pipe(cssmin())
